ef61ac7fb7c86cfff43f15c72bbdc5348d00baf0
Dmitriy2311 <118358946+Dmitriy2311@users.noreply.github.com>
2022-12-22 22:43:39 +0300

    Add files via upload

---
+++ yatube/posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index, name='index'),
+    # Список групп
+    path('group/<slug:slug>/',
+         views.group_posts,
+         name='group_list'
+         )
+]
