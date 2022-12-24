5c428f675c1958bc232d7393a0b48f99e61056bc
Dmitriy2311 <118358946+Dmitriy2311@users.noreply.github.com>
2022-12-24 18:14:59 +0300

    Add files via upload

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='group'
+        related_name='posts'
     )
 
     class Meta:
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,13 +7,13 @@ POST_LIMIT = 10
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()[:POST_LIMIT]
-    return render(request, 'posts/index.html', {"posts": posts})
+    post = Post.objects.all()[:POST_LIMIT]
+    return render(request, 'posts/index.html', {"posts": post})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.group.all()[:POST_LIMIT]
+    posts = group.posts.all()[:POST_LIMIT]
     return render(
         request,
         'posts/group_list.html',
